85ef8c3f8fe01c2d123d9b5f3660f7238a06833b
Laura <laura.kan@hotmail.com>
2021-07-15 09:58:41 +1000

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array_of_strings)
+        array_of_strings.select do |string|
+            string.chars.sort == word.chars.sort
+    end
+end
+
+listen = Anagram.new("listen")
+end
